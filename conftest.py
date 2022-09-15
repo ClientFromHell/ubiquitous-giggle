cce2238d4fc24eeb5c460da55529bfda03cacd0f
ClientFromHell <s.zheludev@gmail.com>
2022-09-15 19:12:19 +0300

    File with fuxture

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose browser preferred language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    lang = request.config.getoption("language")
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('prefs', {'intl.accept_languages': lang})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
