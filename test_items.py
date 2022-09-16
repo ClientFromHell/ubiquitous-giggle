fd64fdc6d9dc806f577896819b6e74e446c1b5a8
ClientFromHell <s.zheludev@gmail.com>
2022-09-16 14:27:39 +0300

    File with test

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, InvalidSelectorException
 import time
 
 url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -9,13 +9,17 @@ url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_if_add_to_cart_button_is_available(browser):
     browser.get(url)
-    time.sleep(30)
+    time.sleep(3)
     try:
         button = WebDriverWait(browser, 3).until(
-            EC.visibility_of_all_elements_located((By.CLASS_NAME, "btn-add-to-basket"))
+            EC.visibility_of_all_elements_located((By.CLASS_NAME, "1btn-add-to-basket"))
         )
-    except TimeoutException:
+    except (TimeoutException, InvalidSelectorException):
         button = None
     assert button, 'The button is not available on this page'
-
-
+    #
+    #
+    # time.sleep(1)
+    # browser.implicitly_wait(5)
+    # button = browser.find_elements(By.CSS_SELECTOR, '.a1dd-to-basket>button')
+    # assert button, "NOT FOUND"
